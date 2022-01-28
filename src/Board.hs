bc5239535490a6637e50fffe34d8e08bdced20a7
CasualSpy <arnaudbegin@hotmail.com>
2022-01-27 21:50:58 -0500

    Basic datatypes to represent the board

--- src/Board.hs
+++ src/Board.hs
@@ -1,5 +1,7 @@
 module Board where
 
+import Data.Array.IArray
+
 data PieceType
   = MovedPawn EnPassantStatus -- EnPassantStatus tracks whether a pawn can be taken En Passant
   | UnmovedPawn -- Unmoved pawns can move differently from moved pawns
@@ -17,3 +19,11 @@ newtype EnPassantStatus = EnPassantStatus Bool deriving (Eq)
 data Color = Black | White deriving (Eq)
 
 data Piece = Piece Color PieceType deriving (Eq)
+
+data Rank = A | B | C | D | E | F | G | H deriving (Show, Eq, Enum, Ord, Ix)
+
+data File = One | Two | Three | Four | Five | Six | Seven | Eight deriving (Show, Eq, Enum, Ord, Ix)
+
+type Square = (Rank, File)
+
+type Board = Array Square (Maybe Piece)
